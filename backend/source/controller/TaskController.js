e8bc77a5765e6cef9e280f3980d2025730f4a860
André Martins <andre.lucas.medeir@gmail.com>
2021-06-19 23:17:18 -0300

    Adicionei as rotas dos filtros de hoje, semana, mês e ano

--- backend/source/controller/TaskController.js
+++ backend/source/controller/TaskController.js
@@ -1,4 +1,5 @@
 const TaskModel = require('../model/TaskModel');
+const {startOfDay, endOfDay, startOfWeek, endOfWeek, startOfMonth, endOfMonth, startOfYear, endOfYear} = require('date-fns');
 
 const timeCurrent = new Date();
 
@@ -17,17 +18,6 @@ class TaskController {
 
     async update(request, response) {
         await TaskModel.findByIdAndUpdate({'_id': request.params.id}, request.body, {new: true})
-            .then(res => {
-                return response.status(200).json(res);
-            })
-            .catch(error => {
-                return response.status(500).json(error); 
-            });
-    }
-
-    async all(request, response) {
-        await TaskModel.find({ macAddress: {'$in': request.body.macAddress}})
-            .sort('when')
             .then(res => {
                 return response.status(200).json(res);
             })
@@ -80,6 +70,61 @@ class TaskController {
 
     async late(request, response) {
         await TaskModel.find({'when': {'$lt': timeCurrent}, 'macAddress': {'$in': request.body.macAddress}})
+            .sort('when')
+            .then(res => {
+                return response.status(200).json(res);
+            })
+            .catch(error => {
+                return response.status(500).json(error); 
+            });
+    }
+
+    async all(request, response) {
+        await TaskModel.find({ macAddress: {'$in': request.body.macAddress}})
+            .sort('when')
+            .then(res => {
+                return response.status(200).json(res);
+            })
+            .catch(error => {
+                return response.status(500).json(error); 
+            });
+    }
+
+    async today(request, response) {
+        await TaskModel.find({'when': {'$gte': startOfDay(timeCurrent), '$lt': endOfDay(timeCurrent)}, 'macAddress': {'$in': request.body.macAddress}})
+            .sort('when')
+            .then(res => {
+                return response.status(200).json(res);
+            })
+            .catch(error => {
+                return response.status(500).json(error); 
+            });
+    }
+
+    async week(request, response) {
+        await TaskModel.find({'when': {'$gte': startOfWeek(timeCurrent), '$lt': endOfWeek(timeCurrent)}, 'macAddress': {'$in': request.body.macAddress}})
+            .sort('when')
+            .then(res => {
+                return response.status(200).json(res);
+            })
+            .catch(error => {
+                return response.status(500).json(error); 
+            });
+    }
+
+    async month(request, response) {
+        await TaskModel.find({'when': {'$gte': startOfMonth(timeCurrent), '$lt': endOfMonth(timeCurrent)}, 'macAddress': {'$in': request.body.macAddress}})
+            .sort('when')
+            .then(res => {
+                return response.status(200).json(res);
+            })
+            .catch(error => {
+                return response.status(500).json(error); 
+            });
+    }
+
+    async year(request, response) {
+        await TaskModel.find({'when': {'$gte': startOfYear(timeCurrent), '$lt': endOfYear(timeCurrent)}, 'macAddress': {'$in': request.body.macAddress}})
             .sort('when')
             .then(res => {
                 return response.status(200).json(res);
--- backend/source/routes/TaskRoutes.js
+++ backend/source/routes/TaskRoutes.js
@@ -10,8 +10,12 @@ router.put('/:id', TaskValidation, TaskController.update);
 router.get('/:id', TaskController.show);
 router.delete('/:id', TaskController.delete);
 
-router.get('/filter/all', MacValidation, TaskController.all);
 router.get('/filter/late', MacValidation, TaskController.late);
+router.get('/filter/all', MacValidation, TaskController.all);
+router.get('/filter/today', MacValidation, TaskController.today);
+router.get('/filter/week', MacValidation, TaskController.week);
+router.get('/filter/month', MacValidation, TaskController.month);
+router.get('/filter/year', MacValidation, TaskController.year);
 
 router.put('/:id/:conclude', TaskController.conclude);
 
