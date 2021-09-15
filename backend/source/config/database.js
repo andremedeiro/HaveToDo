57178e5f977d7195ded97efd50130c53b1c6da63
André Martins <andre.lucas.medeir@gmail.com>
2021-09-15 11:47:31 -0300

    Preparando Back para por online

--- backend/source/config/database.js
+++ backend/source/config/database.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
-const url = 'mongodb://localhost:27017/HaveToDo';
+const mongo_url = process.env.MONGO_URL
+
+const url = mongo_url;
 mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true });
 
 module.exports = mongoose;
